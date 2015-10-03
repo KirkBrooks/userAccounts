22f9432416090e3f42cab147660e210da586debe
Kirk Brooks <kirk.brooks@gmail.com>
2015-10-03 10:42:51 -0700

    Update to Meteor 1.2
    and new Meteor Toys

--- client/app/index/nav.js
+++ client/app/index/nav.js
@@ -6,15 +6,23 @@ Template.navSearch.events({
 	'click #btn-search': function (event, tpl) {
 		 event.preventDefault();
 		$('[id=search-form]').toggle();
-
 	},
 	'blur #searchTerm': function (event) {
 		var srchTerm = $('[name=searchTerm]').val();
+		var lastTerm = Session.get('srchTerm');
 		var isViz = $('[id=search-form]').css("display")
 		
 		if (isViz && srchTerm) {
-			sAlert.info("This would search for '" + srchTerm + "'.");
+			var doSrch = true;
+			// is it the same term as before?
+			if (lastTerm == srchTerm) { doSrch = confirm ("Do that search again?");} 
+			if( doSrch == true ) {
+				sAlert.info("This would search for '" + srchTerm + "'.");
+				Session.set('srchTerm', srchTerm);
 			}
+		} else {
+			$('[id=search-form]').toggle();				
+		}
 	}
 });
 
